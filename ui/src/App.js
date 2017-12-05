e45c8a03a3d9a3dc0dbead7126d19188a7c28e99
Rafael Schloming <rhs@alum.mit.edu>
2017-12-05 17:00:40 -0500

    changed logo; added metrics

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './jetpack.svg';
 import './App.css';
 
 import 'semantic-ui-css/semantic.min.css';
-import { Grid } from 'semantic-ui-react'
+import { Card, Grid, Image } from 'semantic-ui-react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+class Metrics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "created": {},
+      "closed": {},
+      "releases": {}
+    }
+  }
+
+  componentDidMount() {
+    fetch("/api/metrics", {credentials: "same-origin"}).then(response => response.json()).then(data => {
+      this.setState(prev => data);
+    });
+  }
+
+  render() {
+    var data = [];
+
+    var title = this.props.title;
+    var project = this.props.project;
+
+    var created = this.state.created["datawire/" + project];
+    var closed = this.state.closed["datawire/" + project];
+    var releases = this.state.releases[project];
+
+    if (created && closed && releases) {
+      for (var wn = 49; wn > 0; wn--) {
+        var item = {
+          "name": "Week " + wn,
+          "opened": (wn in created) ? created[wn].length : 0,
+          "closed": (wn in closed) ? closed[wn].length : 0,
+          "releases": (wn in releases) ? releases[wn].length : 0
+        };
+        data.unshift(item);
+        if (data.length > 12) {
+          break;
+        }
+      }
+    }
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <ResponsiveContainer width="100%" height={200} >
+        <BarChart data={data} barGap="0" margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+          <XAxis dataKey="name"/>
+          <YAxis/>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <Tooltip/>
+          <Legend />
+          <Bar dataKey="opened" fill="#aa0000" />
+          <Bar dataKey="closed" fill="#00aa00" />
+          <Bar dataKey="releases" fill="#0000aa" />
+        </BarChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  }
+}
 
 class Poller extends React.Component {
   constructor(props) {
@@ -22,11 +84,14 @@ class Poller extends React.Component {
   }
 
   poll() {
+    var start = new Date().getTime();
     fetch(this.props.endpoint, {credentials: "same-origin"}).then(response => response.text()).then(text => {
       this.setState(prev => {
-        var responses = prev.responses.concat([text]);
+        var now = new Date().getTime();
+        var elapsed = now - start;
+        var responses = prev.responses.concat([{latency: elapsed, body: text}]);
         var n = responses.length;
-        var max = 3;
+        var max = 1000;
         if (n > max) {
           responses = responses.slice(n - max, n);
         }
@@ -40,36 +105,54 @@ class Poller extends React.Component {
 
   render() {
     var items = [];
-    for (var i = 0 ; i < this.state.responses.length; i++) {
-      items.push(<p>{this.state.responses[i]}</p>);
+    for (var i = this.state.responses.length - 1; i >= 0; i--) {
+      items.push(<p>{this.state.responses[i].body}</p>);
+      if (items.length > 2) {
+        break;
+      }
     }
+
+    var sum = 0;
+    var count = this.state.responses.length;
+    for (var j = 0; j < count; j++) {
+      var response = this.state.responses[j];
+      sum += response.latency;
+    }
+    var avg = (sum/count).toFixed(2);
+
     return (
-        <ul>{items}</ul>
+        <Card>
+        <Card.Content header>{this.props.endpoint}</Card.Content>
+        <Card.Content description>{items}</Card.Content>
+        <Card.Content extra>latency: {avg}</Card.Content>
+        </Card>
     );
   }
 }
 
 const GridExampleCelled = () => (
-  <Grid celled>
+  <Grid celled columns={15}>
     <Grid.Row>
-      <Grid.Column width={3}>
-        Average service latency
+      <Grid.Column width={5}>
+        <Metrics title="Ambassador" project="ambassador"/>
       </Grid.Column>
-      <Grid.Column width={13}>
-        Number of open GitHub issues:
-        <Poller endpoint="issues"/>
+      <Grid.Column width={5}>
+        <Metrics title="Forge" project="forge"/>
+      </Grid.Column>
+      <Grid.Column width={5}>
+        <Metrics title="Telepresence" project="telepresence"/>
       </Grid.Column>
     </Grid.Row>
 
     <Grid.Row>
-      <Grid.Column width={3}>
+      <Grid.Column width={5}>
         <Poller endpoint="updates"/>
       </Grid.Column>
-      <Grid.Column width={10}>
+      <Grid.Column width={5}>
         <Poller endpoint="api"/>
       </Grid.Column>
-      <Grid.Column width={3}>
-        <p>This is a test.</p>
+      <Grid.Column width={5}>
+        <Poller endpoint="issues"/>
       </Grid.Column>
     </Grid.Row>
   </Grid>
@@ -80,7 +163,7 @@ class App extends Component {
     return (
       <div className="app">
         <div style={{height: "60px"}}/>
-        <h1 className="title">Twitface</h1>
+        <Image className="logo" centered size="small" src={logo} />
         <div style={{height: "60px"}}/>
         <div style={{width: "90%", margin: "0 auto"}}>
           <GridExampleCelled/>
