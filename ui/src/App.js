01b4a7b9facf493a7ce11d89302ef8464c38979d
Rafael Schloming <rhs@alum.mit.edu>
2017-12-19 16:16:31 -0500

    Use the current week number instead of hardcoding the metrics to week 49.

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,6 +6,27 @@ import 'semantic-ui-css/semantic.min.css';
 import { Card, Grid, Image, Table } from 'semantic-ui-react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+// Source: https://weeknumber.net/how-to/javascript
+// Returns the ISO week of the date.
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+   date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+// Returns the four-digit year corresponding to the ISO week of the date.
+Date.prototype.getWeekYear = function() {
+  var date = new Date(this.getTime());
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  return date.getFullYear();
+}
+
 class Metrics extends React.Component {
   constructor(props) {
     super(props);
@@ -35,7 +56,7 @@ class Metrics extends React.Component {
     var releases = this.state.releases[project];
 
     if (created && closed && releases) {
-      for (var wn = 49; wn > 0; wn--) {
+      for (var wn = new Date().getWeek(); wn > 0; wn--) {
         var item = {
           "name": "Week " + wn,
           "opened": (wn in created) ? created[wn].length : 0,
