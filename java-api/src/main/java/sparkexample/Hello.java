ec43dc5ef1ffbd9a95003225e6c64686e44034bf
Philip Lombardi <plombardi@datawire.io>
2017-12-21 10:32:42 -0500

    upgrade spark to 2.7

--- java-api/src/main/java/sparkexample/Hello.java
+++ java-api/src/main/java/sparkexample/Hello.java
@@ -2,8 +2,7 @@ package sparkexample;
 
 import java.util.concurrent.TimeUnit;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
+import static spark.Spark.*;
 
 public class Hello {
 
@@ -11,6 +10,8 @@ public class Hello {
 
     public static void main(String[] args) {
         port(8080);
+        ipAddress("0.0.0.0");
+
         get("/", (req, res) -> {
             long elapsed = System.currentTimeMillis() - start;
             String uptime = String.format("%d:%d:%d",
