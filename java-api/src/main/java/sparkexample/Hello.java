0edf465c8d21dc0c49e6063c8489292805afaa8e
Rafael Schloming <rhs@alum.mit.edu>
2017-12-20 21:42:17 -0500

    Fix spark impl and make it look like python.

--- java-api/src/main/java/sparkexample/Hello.java
+++ java-api/src/main/java/sparkexample/Hello.java
@@ -1,14 +1,23 @@
 package sparkexample;
 
+import java.util.concurrent.TimeUnit;
+
 import static spark.Spark.get;
 import static spark.Spark.port;
 
 public class Hello {
 
+    private static long start = System.currentTimeMillis();
+
     public static void main(String[] args) {
         port(8080);
         get("/", (req, res) -> {
-            return "hello from aa aa sparkjava.com";
+            long elapsed = System.currentTimeMillis() - start;
+            String uptime = String.format("%d:%d:%d",
+                                          TimeUnit.MILLISECONDS.toHours(elapsed),
+                                          TimeUnit.MILLISECONDS.toMinutes(elapsed),
+                                          TimeUnit.MILLISECONDS.toSeconds(elapsed));
+            return String.format("Hello, Spark! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
         });
     }
 
